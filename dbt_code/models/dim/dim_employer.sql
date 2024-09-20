8e1057ab241f7adb13cd9cb3b3377154094ad30f
Attapon <18attaponsayod@gmail.com>
2024-09-20 12:53:09 +0200

    modeling shit

--- dbt_code/models/dim/dim_employer.sql
+++ dbt_code/models/dim/dim_employer.sql
@@ -2,6 +2,10 @@ WITH src_employer AS (SELECT * FROM {{ ref('src_employer') }})
 
 SELECT  
     {{ dbt_utils.generate_surrogate_key(['id', 'employer_name']) }} AS employer_id,
-    employer_name,
+    workplace_id,
+    first_name,
+    sur_name,
+    date_of_birth,
+    gender,
     {{ capitalize_firs_letter("coalesce(workplace_city, 'stad ej specificerad')") }} AS workplace_city
 FROM src_employer
